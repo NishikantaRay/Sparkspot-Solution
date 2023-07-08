d1f4fb3a87a165992beaa3c94c578d3f4331b363
NishikantaRay <nishikantaray1@gmail.com>
2023-07-09 03:16:14 +0530

    add

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -158,6 +158,32 @@
             }
         }
     })
+// testimonial Active
+$('.testimonial_list2').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots: true,
+    nav: false,
+    navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        992: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        },
+        1920: {
+            items: 3
+        }
+    }
+})
 
     // testimonial Active
     $('.testi_list').owlCarousel({
